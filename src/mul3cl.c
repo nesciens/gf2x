4d62e86c96dfbb9d2b4c833d90880e2d4ed00ff4
gaudry <gaudry@e5c1114b-a573-4582-9dac-f72f410959ce>
2010-09-09 15:17:40 +0000

    activate mul3cl in tuning
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/gf2x/trunk@47 e5c1114b-a573-4582-9dac-f72f410959ce

--- src/mul3cl.c
+++ src/mul3cl.c
@@ -1,6 +1,6 @@
 /* This file is part of the gf2x library.
 
-   Copyright 2007, 2008, 2009
+   Copyright 2007, 2008, 2009, 2010
    Richard Brent, Pierrick Gaudry, Emmanuel Thome', Paul Zimmermann
 
    This program is free software; you can redistribute it and/or modify it
@@ -18,32 +18,59 @@
    the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
    Boston, MA 02110-1301, USA.
 */
+/* Implements 128x128 -> 256 bit product using pclmulqdq instruction. */
+
 #ifndef GF2X_MUL3_H_
 #define GF2X_MUL3_H_
 
 #include "gf2x.h"
+#include "gf2x/gf2x-impl.h"
 /* All gf2x source files for lowlevel functions must include gf2x-small.h
  * This is mandatory for the tuning mechanism. */
 #include "gf2x/gf2x-small.h"
 
-/* specialized Karatsuba */
+#include <stdint.h>
+#include <wmmintrin.h>
+
+#if GF2X_WORDSIZE != 64
+#error "This code is for 64-bit only"
+#endif
+
+#include "gf2x/gf2x-config.h"
+
+#ifndef HAVE_PCLMUL_SUPPORT
+#error "This code needs pclmul support"
+#endif
+
+#ifdef  TUNING
+#undef  GF2X_STORAGE_CLASS_mul3
+#define GF2X_STORAGE_CLASS_mul3 /**/
+#endif
+
 GF2X_STORAGE_CLASS_mul3
 void gf2x_mul3 (unsigned long *c, const unsigned long *a, const unsigned long *b)
 {
-  unsigned long aa[2], bb[2], ab[4], c24, c35;
-  gf2x_mul1 (c + 4, a[2], b[2]);
-  gf2x_mul2 (c, a, b);
-  aa[0] = a[0] ^ a[2];
-  aa[1] = a[1];
-  bb[0] = b[0] ^ b[2];
-  bb[1] = b[1];
-  c24 = c[2] ^ c[4];
-  c35 = c[3] ^ c[5];
-  gf2x_mul2 (ab, aa, bb);
-  c[2] = ab[0] ^ c[0] ^ c24;
-  c[3] = ab[1] ^ c[1] ^ c35;
-  c[4] = ab[2] ^ c24;
-  c[5] = ab[3] ^ c35;
+  unsigned long aa[3], bb[3];
+  unsigned long p0[2], p1[2], p2[2];
+  unsigned long pp0[2], pp1[2], pp2[2];
+  aa[0] = a[1]^a[2];
+  aa[1] = a[0]^a[2];
+  aa[2] = a[0]^a[1];
+  bb[0] = b[1]^b[2];
+  bb[1] = b[0]^b[2];
+  bb[2] = b[0]^b[1];
+  gf2x_mul1 (p0, a[0], b[0]);
+  gf2x_mul1 (p1, a[1], b[1]);
+  gf2x_mul1 (p2, a[2], b[2]);
+  gf2x_mul1 (pp0, aa[0], bb[0]);
+  gf2x_mul1 (pp1, aa[1], bb[1]);
+  gf2x_mul1 (pp2, aa[2], bb[2]);
+  c[0] = p0[0];
+  c[1] = p0[0]^p1[0]^pp2[0]         ^  p0[1];
+  c[2] = p0[0]^p1[0]^p2[0]^pp1[0]   ^  p0[1]^p1[1]^pp2[1];
+  c[3] = pp0[0]^p1[0]^p2[0]         ^  p0[1]^p1[1]^p2[1]^pp1[1];
+  c[4] = p2[0]                      ^  pp0[1]^p1[1]^p2[1];
+  c[5] =                               p2[1];
 }
 
 #endif  /* GF2X_MUL3_H_ */
