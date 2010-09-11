1ac7b6b845adba44d5ece02de8734ff9592e5952
thome <thome@e5c1114b-a573-4582-9dac-f72f410959ce>
2010-09-11 15:24:25 +0000

    removed unneeded lines and dependencies in mul3k2.c
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/gf2x/trunk@61 e5c1114b-a573-4582-9dac-f72f410959ce

--- src/mul3k2.c
+++ src/mul3k2.c
@@ -23,24 +23,9 @@
 #define GF2X_MUL3_H_
 
 #include "gf2x.h"
-#include "gf2x/gf2x-impl.h"
 /* All gf2x source files for lowlevel functions must include gf2x-small.h
  * This is mandatory for the tuning mechanism. */
 #include "gf2x/gf2x-small.h"
-
-#include <stdint.h>
-#include <wmmintrin.h>
-
-#if GF2X_WORDSIZE != 64
-#error "This code is for 64-bit only"
-#endif
-
-#include "gf2x/gf2x-config.h"
-
-#ifdef  TUNING
-#undef  GF2X_STORAGE_CLASS_mul3
-#define GF2X_STORAGE_CLASS_mul3 /**/
-#endif
 
 /* uses the variant of Karatsuba with 6 multiplications */
 GF2X_STORAGE_CLASS_mul3
