ea2ab6f32aaecfb2886901b1112895c1b5adcfe1
thome <thome@e5c1114b-a573-4582-9dac-f72f410959ce>
2010-09-13 12:19:58 +0000

    fixed a few copyright strings
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/gf2x/trunk@82 e5c1114b-a573-4582-9dac-f72f410959ce

--- src/tuning_undefs_pre.h
+++ src/tuning_undefs_pre.h
@@ -1,3 +1,23 @@
+/* This file is part of the gf2x library.
+
+   Copyright 2007, 2008, 2009, 2010
+   Richard Brent, Pierrick Gaudry, Emmanuel Thome', Paul Zimmermann
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation; either version 2.1 of the License, or (at
+   your option) any later version.
+   
+   This program is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+   License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public
+   License along with CADO-NFS; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
 #ifndef TUNING_UNDEFS_@@SIZE@@_H_
 #define TUNING_UNDEFS_@@SIZE@@_H_
 
