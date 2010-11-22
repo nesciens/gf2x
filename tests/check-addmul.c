0b6c6e1771afcda43f3270d947ad60f7bcf459a9
thome <thome@e5c1114b-a573-4582-9dac-f72f410959ce>
2010-11-22 12:30:22 +0000

    silenced last warnings.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/gf2x/trunk@116 e5c1114b-a573-4582-9dac-f72f410959ce

--- tests/check-addmul.c
+++ tests/check-addmul.c
@@ -76,7 +76,7 @@ check_gf2x_mul_1_n (long sb)
 int main(int argc, char * argv[])
 {
     if (argc != 1) {
-        fprintf(stderr, "Usage: ./check-addmul\n");
+        fprintf(stderr, "Usage: %s\n", argv[0]);
         exit(1);
     }
 
