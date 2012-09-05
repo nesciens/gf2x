b17b764dce4bce272387671f8910cce9ed08191d
thome <thome@e5c1114b-a573-4582-9dac-f72f410959ce>
2012-09-05 14:16:13 +0000

    make mul2t2 more intel-intrinsics friendly. This happens to improve the performance a great deal with gcc-4.6, at least when using -march=native
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/gf2x/trunk@144 e5c1114b-a573-4582-9dac-f72f410959ce

--- src/mul2t2.c
+++ src/mul2t2.c
@@ -55,6 +55,19 @@
  * upgrade to 4.3.2 */
 #endif
 
+/* This code has been modified in comparison to the previously existing
+ * one, in order to use Intel intrinsics exclusively, and not rely on gcc
+ * syntax.
+ *
+ * The Intel C++ Intrinsics Reference is a good read. However I can't
+ * find the official link right now. An old version can be found in the wild:
+ *
+ * http://www.info.univ-angers.fr/~richer/ens/l3info/ao/intel_intrinsics.pdf
+ *
+ * I don't even know if this document evolved. Probably not.
+ *
+ * It says: ``Document Number: 312482-003US''
+ */
 GF2X_STORAGE_CLASS_mul2
 void gf2x_mul2(unsigned long * t, unsigned long const * s1,
         unsigned long const * s2)
@@ -63,79 +76,88 @@ void gf2x_mul2(unsigned long * t, unsigned long const * s1,
 #define SHR(x, r) _mm_srli_epi64((x), (r))
 #define SHLD(x, r) _mm_slli_si128((x), (r) >> 3)
 #define SHRD(x, r) _mm_srli_si128((x), (r) >> 3)
-    __v2di u;
-    __v2di t0;
-    __v2di t1;
-    __v2di t2;
+#define XOREQ(lop, rop) lop = _mm_xor_si128((lop), (rop))
+#define PXOR(lop, rop) _mm_xor_si128((lop), (rop))
+#define PAND(lop, rop) _mm_and_si128((lop), (rop))
 
-    __v2di g[16];
-    __v2di w;
-    __v2di m = (__v2di) { 0xeeeeeeeeeeeeeeeeUL, 0xeeeeeeeeeeeeeeeeUL, };
+    __m128i u;
+    __m128i t0;
+    __m128i t1;
+    __m128i t2;
+
+    __m128i g[16];
+    __m128i w;
+    // __m128i m = _mm_set1_epi32(0xeeeeeeee);
+#if defined(__GNUC__) && __GNUC__ == 4 &&__GNUC_MINOR__ == 1
+    __m128i m = _mm_set1_epi64(_mm_cvtsi64x_si64(0xeeeeeeeeeeeeeeeeL));
+#else
+    __m128i m = _mm_set1_epi64(_mm_cvtsi64_m64(0xeeeeeeeeeeeeeeeeL));
+#endif
     /* sequence update walk */
-    g[ 0] = (__v2di) { 0, };
-    __v2di b0 = _mm_loadu_si128((__v2di*) s2);
+    __m128i b0 = _mm_loadu_si128((__m128i*) s2);
+    g[ 0] = _mm_setzero_si128();
     g[ 1] = b0;
-    __v2di v1 = (__v2di) { s1[0], s1[0], };
-    w = -SHR(b0,63);
-    __v2di v2 = (__v2di) { s1[1], s1[1], };
-    v1 = SHR(v1 & m, 1); t1 = v1 & w;
-    g[ 2] = SHL(b0, 1); g[ 3] = g[ 2] ^ b0;
-    v2 = SHR(v2 & m, 1); t2 = v2 & w;
-    g[ 4] = SHL(g[ 2], 1); g[ 5] = g[ 4] ^ b0;
-    w = -SHR(g[ 2],63);
-    g[ 6] = SHL(g[ 3], 1); g[ 7] = g[ 6] ^ b0;
-    v1 = SHR(v1 & m, 1); t1 ^= v1 & w;
-    g[ 8] = SHL(g[ 4], 1); g[ 9] = g[ 8] ^ b0;
-    v2 = SHR(v2 & m, 1); t2 ^= v2 & w;
-    g[10] = SHL(g[ 5], 1); g[11] = g[10] ^ b0;
-    w = -SHR(g[4],63);
-    g[12] = SHL(g[ 6], 1); g[13] = g[12] ^ b0;
-    v1 = SHR(v1 & m, 1); t1 ^= v1 & w;
-    g[14] = SHL(g[ 7], 1); g[15] = g[14] ^ b0;
-    v2 = SHR(v2 & m, 1); t2 ^= v2 & w;
-
+    __m128i v1 = _mm_loadu_si128((__m128i*) s1);
+    w = _mm_sub_epi64(_mm_setzero_si128(), SHR(b0,63));
+    __m128i v2 = _mm_unpackhi_epi64(v1, v1);
+    v1 = _mm_unpacklo_epi64(v1, v1);
+    v1 = SHR(PAND(v1, m), 1); t1 = PAND(v1, w);
+    g[ 2] = SHL(b0, 1); g[ 3] = PXOR(g[ 2], b0);
+    v2 = SHR(PAND(v2, m), 1); t2 = PAND(v2, w);
+    g[ 4] = SHL(g[ 2], 1); g[ 5] = PXOR(g[ 4], b0);
+    w = _mm_sub_epi64(_mm_setzero_si128(), SHR(g[ 2],63));
+    g[ 6] = SHL(g[ 3], 1); g[ 7] = PXOR(g[ 6], b0);
+    v1 = SHR(PAND(v1, m), 1); XOREQ(t1, PAND(v1, w));
+    g[ 8] = SHL(g[ 4], 1); g[ 9] = PXOR(g[ 8], b0);
+    v2 = SHR(PAND(v2, m), 1); XOREQ(t2, PAND(v2, w));
+    g[10] = SHL(g[ 5], 1); g[11] = PXOR(g[10], b0);
+    w = _mm_sub_epi64(_mm_setzero_si128(), SHR(g[4],63));
+    g[12] = SHL(g[ 6], 1); g[13] = PXOR(g[12], b0);
+    v1 = SHR(PAND(v1, m), 1); XOREQ(t1, PAND(v1, w));
+    g[14] = SHL(g[ 7], 1); g[15] = PXOR(g[14], b0);
+    v2 = SHR(PAND(v2, m), 1); XOREQ(t2, PAND(v2, w));
 
 
     /* round 0 */
     u = g[s1[0]       & 15]; t0  = u;
-    u = g[s1[0] >>  4 & 15]; t0 ^= SHL(u,  4); t1 ^= SHR(u, 60);
-    u = g[s1[0] >>  8 & 15]; t0 ^= SHL(u,  8); t1 ^= SHR(u, 56);
-    u = g[s1[0] >> 12 & 15]; t0 ^= SHL(u, 12); t1 ^= SHR(u, 52);
-    u = g[s1[0] >> 16 & 15]; t0 ^= SHL(u, 16); t1 ^= SHR(u, 48);
-    u = g[s1[0] >> 20 & 15]; t0 ^= SHL(u, 20); t1 ^= SHR(u, 44);
-    u = g[s1[0] >> 24 & 15]; t0 ^= SHL(u, 24); t1 ^= SHR(u, 40);
-    u = g[s1[0] >> 28 & 15]; t0 ^= SHL(u, 28); t1 ^= SHR(u, 36);
-    u = g[s1[0] >> 32 & 15]; t0 ^= SHL(u, 32); t1 ^= SHR(u, 32);
-    u = g[s1[0] >> 36 & 15]; t0 ^= SHL(u, 36); t1 ^= SHR(u, 28);
-    u = g[s1[0] >> 40 & 15]; t0 ^= SHL(u, 40); t1 ^= SHR(u, 24);
-    u = g[s1[0] >> 44 & 15]; t0 ^= SHL(u, 44); t1 ^= SHR(u, 20);
-    u = g[s1[0] >> 48 & 15]; t0 ^= SHL(u, 48); t1 ^= SHR(u, 16);
-    u = g[s1[0] >> 52 & 15]; t0 ^= SHL(u, 52); t1 ^= SHR(u, 12);
-    u = g[s1[0] >> 56 & 15]; t0 ^= SHL(u, 56); t1 ^= SHR(u,  8);
-    u = g[s1[0] >> 60 & 15]; t0 ^= SHL(u, 60); t1 ^= SHR(u,  4);
+    u = g[s1[0] >>  4 & 15]; XOREQ(t0, SHL(u,  4)); XOREQ(t1, SHR(u, 60));
+    u = g[s1[0] >>  8 & 15]; XOREQ(t0, SHL(u,  8)); XOREQ(t1, SHR(u, 56));
+    u = g[s1[0] >> 12 & 15]; XOREQ(t0, SHL(u, 12)); XOREQ(t1, SHR(u, 52));
+    u = g[s1[0] >> 16 & 15]; XOREQ(t0, SHL(u, 16)); XOREQ(t1, SHR(u, 48));
+    u = g[s1[0] >> 20 & 15]; XOREQ(t0, SHL(u, 20)); XOREQ(t1, SHR(u, 44));
+    u = g[s1[0] >> 24 & 15]; XOREQ(t0, SHL(u, 24)); XOREQ(t1, SHR(u, 40));
+    u = g[s1[0] >> 28 & 15]; XOREQ(t0, SHL(u, 28)); XOREQ(t1, SHR(u, 36));
+    u = g[s1[0] >> 32 & 15]; XOREQ(t0, SHL(u, 32)); XOREQ(t1, SHR(u, 32));
+    u = g[s1[0] >> 36 & 15]; XOREQ(t0, SHL(u, 36)); XOREQ(t1, SHR(u, 28));
+    u = g[s1[0] >> 40 & 15]; XOREQ(t0, SHL(u, 40)); XOREQ(t1, SHR(u, 24));
+    u = g[s1[0] >> 44 & 15]; XOREQ(t0, SHL(u, 44)); XOREQ(t1, SHR(u, 20));
+    u = g[s1[0] >> 48 & 15]; XOREQ(t0, SHL(u, 48)); XOREQ(t1, SHR(u, 16));
+    u = g[s1[0] >> 52 & 15]; XOREQ(t0, SHL(u, 52)); XOREQ(t1, SHR(u, 12));
+    u = g[s1[0] >> 56 & 15]; XOREQ(t0, SHL(u, 56)); XOREQ(t1, SHR(u,  8));
+    u = g[s1[0] >> 60 & 15]; XOREQ(t0, SHL(u, 60)); XOREQ(t1, SHR(u,  4));
 
     /* round 1 */
-    u = g[s1[1]       & 15]; t1 ^= u;
-    u = g[s1[1] >>  4 & 15]; t1 ^= SHL(u,  4); t2 ^= SHR(u, 60);
-    u = g[s1[1] >>  8 & 15]; t1 ^= SHL(u,  8); t2 ^= SHR(u, 56);
-    u = g[s1[1] >> 12 & 15]; t1 ^= SHL(u, 12); t2 ^= SHR(u, 52);
-    u = g[s1[1] >> 16 & 15]; t1 ^= SHL(u, 16); t2 ^= SHR(u, 48);
-    u = g[s1[1] >> 20 & 15]; t1 ^= SHL(u, 20); t2 ^= SHR(u, 44);
-    u = g[s1[1] >> 24 & 15]; t1 ^= SHL(u, 24); t2 ^= SHR(u, 40);
-    u = g[s1[1] >> 28 & 15]; t1 ^= SHL(u, 28); t2 ^= SHR(u, 36);
-    u = g[s1[1] >> 32 & 15]; t1 ^= SHL(u, 32); t2 ^= SHR(u, 32);
-    u = g[s1[1] >> 36 & 15]; t1 ^= SHL(u, 36); t2 ^= SHR(u, 28);
-    u = g[s1[1] >> 40 & 15]; t1 ^= SHL(u, 40); t2 ^= SHR(u, 24);
-    u = g[s1[1] >> 44 & 15]; t1 ^= SHL(u, 44); t2 ^= SHR(u, 20);
-    u = g[s1[1] >> 48 & 15]; t1 ^= SHL(u, 48); t2 ^= SHR(u, 16);
-    u = g[s1[1] >> 52 & 15]; t1 ^= SHL(u, 52); t2 ^= SHR(u, 12);
-    u = g[s1[1] >> 56 & 15]; t1 ^= SHL(u, 56); t2 ^= SHR(u,  8);
-    u = g[s1[1] >> 60 & 15]; t1 ^= SHL(u, 60); t2 ^= SHR(u,  4);
+    u = g[s1[1]       & 15]; XOREQ(t1, u);
+    u = g[s1[1] >>  4 & 15]; XOREQ(t1, SHL(u,  4)); XOREQ(t2, SHR(u, 60));
+    u = g[s1[1] >>  8 & 15]; XOREQ(t1, SHL(u,  8)); XOREQ(t2, SHR(u, 56));
+    u = g[s1[1] >> 12 & 15]; XOREQ(t1, SHL(u, 12)); XOREQ(t2, SHR(u, 52));
+    u = g[s1[1] >> 16 & 15]; XOREQ(t1, SHL(u, 16)); XOREQ(t2, SHR(u, 48));
+    u = g[s1[1] >> 20 & 15]; XOREQ(t1, SHL(u, 20)); XOREQ(t2, SHR(u, 44));
+    u = g[s1[1] >> 24 & 15]; XOREQ(t1, SHL(u, 24)); XOREQ(t2, SHR(u, 40));
+    u = g[s1[1] >> 28 & 15]; XOREQ(t1, SHL(u, 28)); XOREQ(t2, SHR(u, 36));
+    u = g[s1[1] >> 32 & 15]; XOREQ(t1, SHL(u, 32)); XOREQ(t2, SHR(u, 32));
+    u = g[s1[1] >> 36 & 15]; XOREQ(t1, SHL(u, 36)); XOREQ(t2, SHR(u, 28));
+    u = g[s1[1] >> 40 & 15]; XOREQ(t1, SHL(u, 40)); XOREQ(t2, SHR(u, 24));
+    u = g[s1[1] >> 44 & 15]; XOREQ(t1, SHL(u, 44)); XOREQ(t2, SHR(u, 20));
+    u = g[s1[1] >> 48 & 15]; XOREQ(t1, SHL(u, 48)); XOREQ(t2, SHR(u, 16));
+    u = g[s1[1] >> 52 & 15]; XOREQ(t1, SHL(u, 52)); XOREQ(t2, SHR(u, 12));
+    u = g[s1[1] >> 56 & 15]; XOREQ(t1, SHL(u, 56)); XOREQ(t2, SHR(u,  8));
+    u = g[s1[1] >> 60 & 15]; XOREQ(t1, SHL(u, 60)); XOREQ(t2, SHR(u,  4));
     /* end */
 
     /* store result */
-    _mm_storeu_si128((__v2di*)t, t0 ^ SHLD(t1, 64));
-    _mm_storeu_si128((__v2di*)(t+2), t2 ^ SHRD(t1, 64));
+    _mm_storeu_si128((__m128i*)t, PXOR(t0, SHLD(t1, 64)));
+    _mm_storeu_si128((__m128i*)(t+2), PXOR(t2, SHRD(t1, 64)));
 }
 
 #endif  /* GF2X_MUL2_H_ */
